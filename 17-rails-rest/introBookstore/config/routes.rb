efbf73318284d3dcbb28b1111d767bc856e010cb
danielkaczmarczyk <danielkaczmarczyk2@gmail.com>
2020-04-06 10:42:17 +0100

    17 init

---
+++ 17-rails-rest/introBookstore/config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  resources :books
+  resources :authors
+  resources :captains
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
