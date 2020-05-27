0b123a50de5ecde46cdad0f89079a5d63d3f907b
danielkaczmarczyk <danielkaczmarczyk2@gmail.com>
2020-05-27 15:56:36 +0100

    auth lectures

---
+++ 37-auth/auth_backend/app/controllers/application_controller.rb
@@ -0,0 +1,41 @@
+class ApplicationController < ActionController::API
+  before_action :authorized
+
+  @secret = ENV["RAILS_AUTH_LECTURE_SECRET_KEY"]
+
+  def auth_header
+    request.headers["Authorization"]
+  end
+
+  def encode_token(payload)
+    JWT.encode(payload, @secret)
+  end
+
+  def decoded_token
+    if auth_header
+      token = auth_header.split(" ")[1]
+      begin
+        JWT.decode(token, @secret)[0]
+      rescue JWT::DecodeError
+        nil
+      end
+    end
+  end
+
+  def current_user
+    if decoded_token
+      user_id = decoded_token[0]["user_id"]
+      @user = User.find_by(id: user_id)
+    else
+      nil
+    end
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def authorized
+    render json: { message: "Please log in" }, status: :unauthorized unless logged_in?
+  end
+end
